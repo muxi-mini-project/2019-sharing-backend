72f5f71fc3638d5a7e3480c1deb3ad275ba891c1
MitsuhaOma <2391542095@qq.com>
2020-01-16 11:29:40 +0800

    new file

---
+++ model/fileinfo.go
@@ -0,0 +1,17 @@
+package model
+
+type File struct {
+     FileId int
+     FileUrl string
+     FileName string
+     Format string
+     Content string
+     Subject string
+     College string
+     Grade float32
+     Likes int
+     CollcetNumber int
+     DownloadNumber int
+}
+
+
