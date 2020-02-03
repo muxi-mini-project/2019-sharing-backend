4577a3ec31a2349efd029b70548e4dcfa336141e
MitsuhaOma <2391542095@qq.com>
2020-02-04 00:11:37 +0800

    new file

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/muxi-mini-project/2020-sharing-backend/router"
-    "github.com/muxi-mini-project/2020-sharing-backend/model"
+	"github.com/muxi-mini-project/2020-sharing-backend/model"
+	"github.com/muxi-mini-project/2020-sharing-backend/router"
 )
 
 func main() {
-    model.DB.Init()        // 初始化数据库
-    defer model.DB.Close() // 记得关闭数据库
+	model.DB.Init()        // 初始化数据库
+	defer model.DB.Close() // 记得关闭数据库
 
-    router.InitRouter()    // 初始化路由
-    router.Router.Run()    // 运行
-    fmt.Println("Running... Successful!")
+	router.InitRouter() // 初始化路由
+	router.Router.Run() // 运行
+	fmt.Println("Running... Successful!")
 }
