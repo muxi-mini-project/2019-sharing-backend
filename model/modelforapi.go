1fe6f8f6529e4b2ef99509a10c0663b4adf407ef
MitsuhaOma <2391542095@qq.com>
2020-03-04 21:20:38 +0800

    new file

--- model/modelforapi.go
+++ model/modelforapi.go
@@ -24,3 +24,18 @@ type Messagelist struct {
 	Image_url string `json:"image_url"`
 	Time      string `json:"write_time"`
 }
+
+type Getfile struct {
+	Message  string `json:"message"`
+	Fileinfo File   `json:"file"`
+}
+
+type Downloadfile struct {
+	Message string `json:"message"`
+	Fileurl string `json:"file_url"`
+}
+
+type Getfiles struct {
+	Message string `json:"message"`
+	Files   []File `json:"files"`
+}
