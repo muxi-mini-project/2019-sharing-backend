9c5c125112ce27a3af773757bc9a6cb3c322ca5f
jepril <hongranhxr@163.com>
2020-03-04 19:17:20 +0800

    hxr

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
 	"github.com/muxi-mini-project/2020-sharing-backend/router"
 )
 
+
+// @title sharing
+// @version 1.0
+// @description 资料共享平台
+
+// @host 47.102.120.167:2333
+// @BasePath /sharing/v1/
+
+// @Schemas http
+
 func main() {
 	model.DB.Init()        // 初始化数据库
 	defer model.DB.Close() // 记得关闭数据库
